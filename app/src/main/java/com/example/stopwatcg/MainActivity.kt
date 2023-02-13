dfc02dee9f342ce70a335d1b2c08f4bb65059e6f
mikhailshauchenka <mike1shef@gmail.com>
2023-02-13 21:56:31 +0100

    Initial commit

---
+++ app/src/main/java/com/example/stopwatcg/MainActivity.kt
@@ -0,0 +1,94 @@
+package com.example.stopwatcg
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.SystemClock
+import com.example.stopwatcg.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    var running = false //is the stopwatch running
+    var offset: Long = 0 //base offset for stopwatch
+
+    //Add key Strings for use with Bundle
+    val OFFSET_KEY = "offset"
+    val RUNNING_KEY = "running"
+    val BASE_KEY = "base"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        //Get a reference to stopwatch
+        //stopwatch = findViewById<Chronometer>(R.id.stopwatch)
+
+        if (savedInstanceState != null) {
+            offset = savedInstanceState.getLong(OFFSET_KEY)
+            running = savedInstanceState.getBoolean(RUNNING_KEY)
+            if (running){
+                binding.stopwatch.base = savedInstanceState.getLong(BASE_KEY)
+                binding.stopwatch.start()
+            } else setBaseTime()
+        }
+
+        //The start button starts the stopwatch if it's not starte
+
+        binding.startButton.setOnClickListener {
+            if (!running) {
+                setBaseTime()
+                binding.stopwatch.start()
+                running = true
+            }
+        }
+        //val pauseButton = findViewById<Button>(R.id.pause_button)
+
+        binding.pauseButton.setOnClickListener {
+            if (running) {
+                saveOffset()
+                binding.stopwatch.stop()
+                running = false
+            }
+        }
+
+        //val resetButton = findViewById<Button>(R.id.reset_button)
+        binding.resetButton.setOnClickListener {
+            offset = 0
+            setBaseTime()
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        if(running){
+            saveOffset()
+            binding.stopwatch.stop()
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (running) {
+            setBaseTime()
+            binding.stopwatch.start()
+            offset = 0
+        }
+    }
+
+    override fun onSaveInstanceState(savedInstanceState: Bundle) {
+        savedInstanceState.putLong(OFFSET_KEY, offset)
+        savedInstanceState.putBoolean(RUNNING_KEY, running)
+        savedInstanceState.putLong(BASE_KEY, binding.stopwatch.base)
+        super.onSaveInstanceState(savedInstanceState)
+    }
+
+    fun setBaseTime(){
+        binding.stopwatch.base = SystemClock.elapsedRealtime() - offset
+    }
+
+    fun saveOffset(){
+        offset = SystemClock.elapsedRealtime() - binding.stopwatch.base
+    }
+}
